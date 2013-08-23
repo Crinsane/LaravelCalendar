7e6a703ae35c9abfc3fbcc879192b672671e4102
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2013-08-23 09:19:15 +0200

    Changed default language of language file to english

---
+++ src/lang/en/calendar.php
@@ -0,0 +1,60 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Calendar Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines for the CalendarGenerator
+    |
+    */
+
+    'su'         => 'Su',
+    'mo'         => 'Mo',
+    'tu'         => 'Tu',
+    'we'         => 'We',
+    'th'         => 'Th',
+    'fr'         => 'Fr',
+    'sa'         => 'Sa',
+    'sun'        => 'Sun',
+    'mon'        => 'Mon',
+    'tue'        => 'Tue',
+    'wed'        => 'Wed',
+    'thu'        => 'Thu',
+    'fri'        => 'Fri',
+    'sat'        => 'Sat',
+    'sunday'     => 'Sunday',
+    'monday'     => 'Monday',
+    'tuesday'    => 'Tuesday',
+    'wednesday'  => 'Wednesday',
+    'thursday'   => 'Thursday',
+    'friday'     => 'Friday',
+    'saturday'   => 'Saturday',
+    'jan'        => 'Jan',
+    'feb'        => 'Feb',
+    'mar'        => 'Mar',
+    'apr'        => 'Apr',
+    'may'        => 'May',
+    'jun'        => 'Jun',
+    'jul'        => 'Jul',
+    'aug'        => 'Aug',
+    'sep'        => 'Sep',
+    'oct'        => 'Oct',
+    'nov'        => 'Nov',
+    'dec'        => 'Dec',
+    'january'    => 'January',
+    'february'   => 'February',
+    'march'      => 'March',
+    'april'      => 'April',
+    'mayl'       => 'May',
+    'june'       => 'June',
+    'july'       => 'July',
+    'august'     => 'August',
+    'september'  => 'September',
+    'october'    => 'October',
+    'november'   => 'November',
+    'december'   => 'December'
+
+);
